87138009320628efae51866d6b109c2d412604c8
Eyder <eyder.suarez@nivelics.co>
2022-02-22 14:13:32 -0500

    Proyecto terminado y funcional

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,17 +6,62 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
+  center: [number, number] = [-76.57044872901194, 3.453486095886988];
 
   constructor() { }
 
-  ngOnInit(): void {
-    var map = new mapboxgl.Map({
-      container: 'mapa',
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
+
+  ngAfterViewInit(): void {
+    
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -76.57044872901194, 3.453486095886988 ],
-      zoom: 17
+      center: this.center,
+      zoom: this.zoomLevel
       });
+
+      this.mapa.on('zoom', (ev) => {
+        this.zoomLevel = this.mapa.getZoom();
+      });
+
+      this.mapa.on('zoomend', (ev) => {
+        if (this.mapa.getZoom() > 18) {
+          this.mapa.zoomTo(18);
+        }
+      });
+
+      //?Movimiento del mapa
+      this.mapa.on('move', (event) => {
+        const target = event.target;
+        const {lng, lat} = target.getCenter()
+        this.center = [lng, lat]
+        
+      });
+  }
+
+  zoomOut(){
+    this.mapa.zoomOut();
+    
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
+    
+  }
+
+  zoomCambio(valor: string) {
+    this.mapa.zoomTo(Number(valor));
+ 
   }
 
 }
